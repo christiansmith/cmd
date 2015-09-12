0c32e80f9932725599f32eb36304566626bb2d4d
Scott Corgan <scottcorgan@gmail.com>
2015-09-11 23:40:07 -0700

    start to pull out utils

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,68 +2,25 @@
 
 var asArray = require('as-array')
 var minimist = require('minimist')
+var omit = require('ramda/src/omit')
+var compose = require('ramda/src/compose')
+var map = require('ramda/src/map')
 
 var name = require('./name')
 var use = require('./use')
 var command = require('./command')
 var flag = require('./flag')
+var cmdUtils = require('./utils')
+var getCommands = cmdUtils.getCommands;
+var getFlags = cmdUtils.getFlags;
+var applyFunctions = cmdUtils.applyFunctions;
 
-var map = require('ramda/src/map')
-var compose = require('ramda/src/compose')
-var apply = require('ramda/src/apply')
-var filter = require('ramda/src/filter')
-var prop = require('ramda/src/prop')
-var equals = require('ramda/src/equals')
-var contains = require('ramda/src/contains')
-var head = require('ramda/src/head')
-var or = require('ramda/src/or')
-var not = require('ramda/src/not')
-var omit = require('ramda/src/omit')
-var path = require('ramda/src/path')
-var flip = require('ramda/src/flip')
-var curryN = require('ramda/src/curryN')
-var keys = require('ramda/src/keys')
+exports.name = name
+exports.use = use
+exports.command = command
+exports.flag = flag
 
-var getAlias = path(['value', 'alias'])
-var valueDoesNotHaveAlias = compose(not, getAlias)
-var isCommand = compose(equals('command'), prop('type'))
-var isFlag = compose(equals('flag'), prop('type'))
-var applyFunctions = curryN(3, function applyFunctions (data, flags, com) {
-
-  return compose(
-    map(flip(apply)([data, flags, com.options])),
-    path(['value', 'function'])
-  )(com)
-})
-function containsAlias (data, def) {
-
-  return compose(contains(head(data)), flip(or)([]), getAlias)(def)
-}
-var matchAlias = curryN(2, function matchAlias (data, def) {
-
-  return or(valueDoesNotHaveAlias(def), containsAlias(data, def))
-})
-function getCommands (data) {
-
-  return compose(
-    filter(matchAlias(data)),
-    filter(isCommand)
-  )
-}
-function getFlags (flags, contexts) {
-
-  var matchFlagAliases = compose(
-    matchAlias,
-    keys
-  )
-
-  return compose(
-    filter(matchFlagAliases(flags)), // TODO: need to format flags input
-    filter(isFlag)
-  )
-}
-
-function cli () {
+var cli = exports.cli = function cli () {
 
   // TODO: Need to flatten and merge the contexts
   //       for use with things like use()
@@ -89,14 +46,6 @@ function cli () {
     runFlags(contexts)
     runCommands(contexts)
   }
-}
-
-module.exports = {
-  cli: cli,
-  name: name,
-  use: use,
-  command: command,
-  flag: flag
 }
 
 var f = flag(
--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 var namespace = require('tessed').namespace
 
 var test = namespace('cli')
-test.helpers = test.namespace('helpers')
 test.exports = test.namespace('exports')
 
 test('yes', function (t) {
